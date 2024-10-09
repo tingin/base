2198734ec9b40d5dcf9c3ada626eb7b49689b55d
ting <ting.cn@msn.com>
2024-10-09 22:01:46 +0800

    Update http.go

--- http/http.go
+++ http/http.go
@@ -77,6 +77,35 @@ func Post(url string, params any, response any) error {
 	return PostWithHeader(url, nil, params, response)
 }
 
+func GetWithHeader(url string, headers []HttpHeader, response any) error {
+	req := fasthttp.AcquireRequest()
+	defer fasthttp.ReleaseRequest(req)
+
+	req.Header.SetMethod("GET")
+	req.SetRequestURI(url)
+
+	if headers != nil {
+		for _, header := range headers {
+			req.Header.Set(header.Key, header.Value)
+		}
+	}
+
+	resp := fasthttp.AcquireResponse()
+	defer fasthttp.ReleaseResponse(resp)
+
+	if err := client.Do(req, resp); err != nil {
+		return fmt.Errorf("failed to send request: %w", err)
+	}
+
+	body := resp.Body()
+
+	if err := json.Unmarshal(body, &response); err != nil {
+		return fmt.Errorf("failed to unmarshal response: %w body: %s", err, body)
+	}
+
+	return nil
+}
+
 func Get(url string, response any) error {
 	req := fasthttp.AcquireRequest()
 	defer fasthttp.ReleaseRequest(req)
