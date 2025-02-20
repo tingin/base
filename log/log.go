ff9a322223021cc0313c98d248a7185081b1d73b
ting <ting.cn@msn.com>
2025-02-20 22:40:43 +0800

    Update log.go
    update log

--- log/log.go
+++ log/log.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/donnie4w/go-logger/logger"
 	"github.com/tingin/base/patterns/singleton"
+	"modernc.org/libc/sys/types"
 )
 
 func formatTime(a slog.Attr) slog.Attr {
@@ -32,9 +33,7 @@ var singletonMap = singleton.NewSingletonMap[string, Logger]()
 
 var defaultKey = "Default"
 
-func init() {
-	singletonMap.AddFactory(defaultKey, defaultInstance)
-}
+var type = "json"
 
 func AddFactory(key string, factory func() *Logger) {
 	singletonMap.AddFactory(key, factory)
@@ -73,28 +72,48 @@ func NewLogger(options Options) *Logger {
 		},
 	},
 	)
-
-	slogger := slog.New(slog.NewJSONHandler(loggingFile, &slog.HandlerOptions{
-		AddSource: true,
-		Level:     slog.LevelInfo,
-		ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
-			switch a.Key {
-			case slog.TimeKey:
-				return formatTime(a)
-			case slog.SourceKey:
-				return formatSource(a)
-			default:
-				return a
-			}
-		},
-	}))
-
+	if (types == "json"){
+		slogger := slog.New(slog.NewJSONHandler(loggingFile, &slog.HandlerOptions{
+			AddSource: true,
+			Level:     slog.LevelInfo,
+			ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
+				switch a.Key {
+				case slog.TimeKey:
+					return formatTime(a)
+				case slog.SourceKey:
+					return formatSource(a)
+				default:
+					return a
+				}
+			},
+		}))	
+	}else{
+		slogger := slog.New(&slog.TextHandler(loggingFile, &slog.HandlerOptions{
+			AddSource: true,
+			Level:     slog.LevelInfo,
+			ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
+				switch a.Key {
+				case slog.TimeKey:
+					return formatTime(a)
+				case slog.SourceKey:
+					return formatSource(a)
+				default:
+					return a
+				}
+			},
+		}))	
+	}
+	
 	return &Logger{
 		slogger: slogger.With("PID", os.Getpid()),
 	}
 }
 
 func Default() *Logger {
+	o := singletonMap.GetInstance(defaultKey)
+	if o == nil {
+		AddFactory(defaultKey, defaultInstance)
+	}
 	return singletonMap.GetInstance(defaultKey)
 }
 
@@ -102,18 +121,20 @@ type Logger struct {
 	slogger *slog.Logger
 }
 
-func (l Logger) InfoJson(msg string, attrs ...slog.Attr) {
-	l.slogger.LogAttrs(context.Background(), slog.LevelInfo, msg, attrs...)
-}
-
-func (l Logger) ErrorJson(msg string, attrs ...slog.Attr) {
-	l.slogger.LogAttrs(context.Background(), slog.LevelError, msg, attrs...)
-}
-
 func (l Logger) Info(msg string, args ...any) {
-	l.slogger.Info(msg, args...)
+	if types=="json"{
+		l.slogger.LogAttrs(context.Background(), slog.LevelInfo, msg, attrs...)
+	}else{
+		l.slogger.Info(msg, args...)
+	}
+	
 }
 
 func (l Logger) Error(msg string, args ...any) {
-	l.slogger.Error(msg, args...)
+	if types=="json"{
+		l.slogger.LogAttrs(context.Background(), slog.LevelError, msg, attrs...)
+		}else{
+			l.slogger.Error(msg, args...)
+		}
+	
 }
